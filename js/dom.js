4e30c50cac29bad7eb8198a553e40014ddc2b708
VladymyrKnysh <99245967+VladymyrKnysh@users.noreply.github.com>
2022-07-31 14:48:21 +0300

    конспект

--- js/dom.js
+++ js/dom.js
@@ -1,25 +1,9 @@
+const list = document.querySelector(".list");
 
-const text = document.querySelector("#paragraph");
+const newTechnologies = ["React", "TypeScript", "Node.js"];
+const markup = newTechnologies
+  .map((technology) => `<li class="list-item new">${technology}</li>`)
+  .join("");
 
-console.log(text.classList); // ["text", "red", "big", value: "text red big"]
-
-console.log(text.classList.contains("red")); // true
-
-text.classList.remove("big");
-console.log(text.classList); // ["text", "red", value: "text red"]
-
-text.classList.add("new-class");
-console.log(text.classList); // ["text", "red", "new-class", value: "text red new-class"]
-
-// Can add multiple classes
-text.classList.add("a", "b", "c");
-console.log(text.classList);
-
-text.classList.toggle("is-hidden"); // will add is-hidden class
-text.classList.toggle("is-hidden"); // will remove is-hidden class
-
-// classList has a forEach method
-text.classList.forEach(cls => {
-  console.log(cls); // text, red, new-class
-});
-
+list.insertAdjacentHTML("beforeend", markup);
+list.insertAdjacentHTML("beforebegin", "<h1>Popular technologies</h1>");
