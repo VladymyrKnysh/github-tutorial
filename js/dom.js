f7332343032702e9de0bb283925a3a5fed84a62b
VladymyrKnysh <99245967+VladymyrKnysh@users.noreply.github.com>
2022-08-05 23:02:09 +0300

    практика

--- js/dom.js
+++ js/dom.js
@@ -1,44 +1,24 @@
+const form = document.querySelector('.login-form')
 
-// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
-// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>)
+form.addEventListener('input', onFormInput)
+form.addEventListener('submit', onFormSubmit)
 
-// Number of categories: 3
-
-// Category: Animals
-// Elements: 4
-
-// Category: Products
-// Elements: 3
-
-// Category: Technologies
-// Elements: 5
-
-const listEl = document.querySelector('#categories')
-
-console.log('Number of categories:', listEl.children.length);
-
-
-const itemsEl = document.querySelectorAll('.item')
-
-itemsEl.forEach(item => {
-    console.log('Category:', item.firstElementChild.textContent);
-    console.log('Elements:', item.lastElementChild.children.length);
-   
-})
-
-
-
-
-
-
-// const titlesEl = document.querySelectorAll('h2')
-
-// titlesEl.forEach((title, index) => {
-//     console.log('Category:', title.textContent);
+const users = []
+ let user = {}
     
-// })
 
-
-// const categoriesLists = document.querySelectorAll('ul')
-// console.log(categoriesLists);
-// console.log(categoriesLists[1].children.length);
+function onFormInput(evt) {
+    evt.preventDefault()
+    
+   
+    user[evt.target.name] = evt.target.value
+    console.log(user);
+}
+ 
+function onFormSubmit(evt) {
+     evt.preventDefault()
+    users.push(user)
+    console.log(users);
+    user = {}
+    evt.target.reset()
+}
